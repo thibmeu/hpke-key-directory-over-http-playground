c2da02f2939349bb7168b1bb8791c96315c9187b
Thibault Meunier <thibault@cloudflare.com>
2025-01-30 13:59:55 +0100

    Actually implement HPKE cose instead of COSE RSA

--- src/directoryHandlers/ietf-hpke-cose-key-set.ts
+++ src/directoryHandlers/ietf-hpke-cose-key-set.ts
@@ -25,13 +25,16 @@ export async function handler(req: Request, env: Bindings): Promise<Response> {
     );
     const publicKeyJwk = (await crypto.subtle.exportKey('jwk', publicKey)) as JsonWebKey;
 
+    // HPKE-1 37
     const publicKeyCoseKeyMap = new Map();
-    publicKeyCoseKeyMap.set(1, 3); // RSA https://www.rfc-editor.org/rfc/rfc8230.html
+    publicKeyCoseKeyMap.set(1, 2); // EC2 https://www.rfc-editor.org/rfc/rfc8230.html
     publicKeyCoseKeyMap.set(2, new Uint8Array([Number.parseInt(metadata.tokenKeyID)]));
-    publicKeyCoseKeyMap.set(3, -38); // RSASSA-PSS SHA-384 https://www.rfc-editor.org/rfc/rfc8230.html
-    publicKeyCoseKeyMap.set(4, [2]); // verify https://datatracker.ietf.org/doc/html/rfc8152#section-7.1
-    publicKeyCoseKeyMap.set(-1, b64Tou8(b64URLtoB64(publicKeyJwk.n!)));
-    publicKeyCoseKeyMap.set(-2, b64Tou8(b64URLtoB64(publicKeyJwk.e!)));
+    publicKeyCoseKeyMap.set(3, 37); // HPKE-1  DHKEM(P-384, HKDF-SHA384) KEM, the HKDF-SHA384 KDF, and the AES-256-GCM AEAD
+    // publicKeyCoseKeyMap.set(4, [3]); // not required for public keys - encrypt https://datatracker.ietf.org/doc/html/rfc8152#section-7.1
+    publicKeyCoseKeyMap.set(-1, 2) // P-384 https://www.iana.org/assignments/cose/cose.xhtml#elliptic-curves
+    // TODO: generate a real key, don't reuse a signature scheme
+    publicKeyCoseKeyMap.set(-2, b64Tou8(b64URLtoB64(publicKeyJwk.n!))); // MUST BE x
+    publicKeyCoseKeyMap.set(-3, b64Tou8(b64URLtoB64(publicKeyJwk.e!))); // MUST BE y
 
     // List of arguments https://www.iana.org/assignments/jose/jose.xhtml
     return publicKeyCoseKeyMap;
